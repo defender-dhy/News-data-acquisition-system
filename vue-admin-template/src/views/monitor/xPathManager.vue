209a4b4cb6a9c315894c278711b996ecf761d83b
roife <roifewu@gmail.com>
2021-07-07 15:13:21 +0800

    enhance xPathManager

--- vue-admin-template/src/views/monitor/xPathManager.vue
+++ vue-admin-template/src/views/monitor/xPathManager.vue
@@ -1,62 +1,199 @@
 <template>
   <el-main>
-    <el-card shadow="never" style="margin-bottom: 20px">
+    <el-card style="margin-bottom: 20px" shadow="never">
+      <div slot="header" class="clearfix">
+        <span>X-Path 管理</span>
+      </div>
       <el-table
         :data="managetableList"
         element-loading-text="Loading"
-        border
         fit
         highlight-current-row
         @cell-dblclick="tableEdit"
       >
-        <el-table-column label="类型" prop="type" sortable/>
-        <el-table-column label="是否在详情页" prop="inSpec" sortable/>
-        <el-table-column label="数据库存储名字" prop="saveName" sortable/>
-        <el-table-column label="需要爬取" prop="needCrawl" sortable/>
-        <el-table-column label="字段名称（英）" prop="EnglishName" sortable/>
-        <el-table-column label="字段名称（中）" prop="ChineseName" sortable/>
-        <el-table-column label="需要存储" prop="needSave" sortable/>
+        <el-table-column label="ID" prop="id" sortable />
+        <el-table-column label="类型" prop="type" sortable />
+        <el-table-column label="是否在详情页" prop="inSpec" sortable />
+        <el-table-column label="数据库存储名字" prop="saveName" sortable />
+        <el-table-column label="需要爬取" prop="needCrawl" sortable />
+        <el-table-column label="字段名称（英）" prop="EnglishName" sortable />
+        <el-table-column label="字段名称（中）" prop="ChineseName" sortable />
+        <el-table-column label="需要存储" prop="needSave" sortable />
       </el-table>
+      <el-row type="flex" justify="center" style="margin-top: 10px">
+        <el-col :span="3">
+          <el-button
+            type="primary"
+            icon="el-icon-circle-plus-outline"
+            size="small"
+            round
+            @click="xPathManagerDialogVisible = true"
+          >
+            新增
+          </el-button>
+        </el-col>
+      </el-row>
     </el-card>
 
-    <el-card shadow="never">
-      <el-table
-        :data="tableList"
-        element-loading-text="Loading"
-        border
-        fit
-        highlight-current-row
-        @cell-dblclick="tableEdit1"
+    <el-dialog title="X-Path 管理" :visible.sync="xPathManagerDialogVisible">
+      <el-form
+        ref="xPathManagerAddForm"
+        :model="xPathManagerAddForm"
+        label-width="150px"
+        :rules="xPathManagerAddFormRules"
       >
-        <el-table-column label="_id" prop="_id" sortable/>
-        <el-table-column label="button_xpath" prop="button_xpath" sortable/>
-        <el-table-column label="column" prop="column" sortable/>
-        <el-table-column label="content_url" prop="content_url" sortable/>
-        <el-table-column label="content_xpath" prop="content_xpath" sortable/>
-        <el-table-column label="lang" prop="lang" sortable/>
-        <el-table-column label="resource_type" prop="resource_type" sortable/>
-        <el-table-column label="time_xpath" prop="time_xpath" sortable/>
-        <el-table-column label="title_xpath" prop="title_xpath" sortable/>
-        <el-table-column label="type" prop="type" sortable/>
-        <el-table-column label="update_interval" prop="update_interval" sortable/>
-        <el-table-column label="web_url" prop="web_url" sortable/>
-        <el-table-column label="website_name" prop="website_name" sortable/>
-        <el-table-column label="writer_xpath" prop="writer_xpath" sortable/>
-        <el-table-column label="article_xpath" prop="article_xpath" sortable/>
-        <el-table-column label="time1_xpath" prop="time1_xpath" sortable/>
-        <el-table-column label="writer1_xpath" prop="writer1_xpath" sortable/>
-        <el-table-column label="prize_name" prop="prize_name" sortable/>
-        <el-table-column label="prize_type" prop="prize_type" sortable/>
-        <el-table-column label="prize_people" prop="prize_people" sortable/>
-        <el-table-column label="prize_org" prop="prize_org" sortable/>
-        <el-table-column label="prize_level" prop="prize_level" sortable/>
-        <el-table-column label="prize_rank" prop="prize_rank" sortable/>
-        <el-table-column label="combine" prop="combine" sortable/>
-        <el-table-column label="source_xpath" prop="source_xpath" sortable/>
-        <el-table-column label="source1_xpath" prop="source1_xpath" sortable/>
-        <el-table-column label="button1_xpath" prop="button1_xpath" sortable/>
-      </el-table>
+        <el-form-item label="id" prop="id">
+          <el-input
+            v-model="xPathManagerAddForm.id"
+            placeholder=""
+            style="width: 300px"
+          />
+        </el-form-item>
+
+        <el-form-item label="类型" prop="type">
+          <el-input
+            v-model="xPathManagerAddForm.type"
+            placeholder=""
+            style="width: 300px"
+          />
+        </el-form-item>
+
+        <el-form-item label="是否在详情页" prop="inSpec">
+          <el-input
+            v-model="xPathManagerAddForm.inSpec"
+            placeholder=""
+            style="width: 300px"
+          />
+        </el-form-item>
+
+        <el-form-item label="数据库存储名字" prop="saveName">
+          <el-input
+            v-model="xPathManagerAddForm.saveName"
+            placeholder=""
+            style="width: 300px"
+          />
+        </el-form-item>
+
+        <el-form-item label="需要爬取" prop="needCrawl">
+          <el-input
+            v-model="xPathManagerAddForm.needCrawl"
+            placeholder=""
+            style="width: 300px"
+          />
+        </el-form-item>
+
+        <el-form-item label="字段名称（英）" prop="EnglishName">
+          <el-input
+            v-model="xPathManagerAddForm.EnglishName"
+            placeholder=""
+            style="width: 300px"
+          />
+        </el-form-item>
+
+        <el-form-item label="字段名称（中）" prop="ChineseName">
+          <el-input
+            v-model="xPathManagerAddForm.ChineseName"
+            placeholder=""
+            style="width: 300px"
+          />
+        </el-form-item>
+
+        <el-form-item label="需要存储" prop="needSave">
+          <el-input
+            v-model="xPathManagerAddForm.needSave"
+            placeholder=""
+            style="width: 300px"
+          />
+        </el-form-item>
+
+        <el-form-item>
+          <el-button
+            type="primary"
+            @click="submitForm('xPathManagerAddForm')"
+          >
+            保存
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+
+    <el-card style="margin-bottom: 20px" shadow="never">
+      <div slot="header" class="clearfix">
+        <span>X-Path 信息</span>
+      </div>
+      <el-form :inline="true">
+        <el-form-item>
+          <el-button type="primary" size="small">上传文档</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="presentType0">显示类型 0</el-checkbox>
+          <el-checkbox v-model="presentType1">显示类型 1</el-checkbox>
+        </el-form-item>
+      </el-form>
+      <div>
+        <el-table
+          :data="tableList.filter(item => item.type === '1' ? presentType1 : presentType0)"
+          element-loading-text="Loading"
+          border
+          fit
+          highlight-current-row
+          @cell-dblclick="tableEdit1"
+        >
+          <el-table-column label="_id" prop="_id" sortable />
+          <el-table-column label="button_xpath" prop="button_xpath" sortable />
+          <el-table-column label="column" prop="column" sortable />
+          <el-table-column label="content_url" prop="content_url" sortable />
+          <el-table-column label="content_xpath" prop="content_xpath" sortable />
+          <el-table-column label="lang" prop="lang" sortable />
+          <el-table-column label="resource_type" prop="resource_type" sortable />
+          <el-table-column label="time_xpath" prop="time_xpath" sortable />
+          <el-table-column label="title_xpath" prop="title_xpath" sortable />
+          <el-table-column label="type" prop="type" sortable />
+          <el-table-column label="update_interval" prop="update_interval" sortable />
+          <el-table-column label="web_url" prop="web_url" sortable />
+          <el-table-column label="website_name" prop="website_name" sortable />
+          <el-table-column label="writer_xpath" prop="writer_xpath" sortable />
+          <el-table-column label="article_xpath" prop="article_xpath" sortable />
+          <el-table-column label="time1_xpath" prop="time1_xpath" sortable />
+          <el-table-column label="writer1_xpath" prop="writer1_xpath" sortable />
+          <el-table-column label="prize_name" prop="prize_name" sortable />
+          <el-table-column label="prize_type" prop="prize_type" sortable />
+          <el-table-column label="prize_people" prop="prize_people" sortable />
+          <el-table-column label="prize_org" prop="prize_org" sortable />
+          <el-table-column label="prize_level" prop="prize_level" sortable />
+          <el-table-column label="prize_rank" prop="prize_rank" sortable />
+          <el-table-column label="combine" prop="combine" sortable />
+          <el-table-column label="source_xpath" prop="source_xpath" sortable />
+          <el-table-column label="source1_xpath" prop="source1_xpath" sortable />
+          <el-table-column label="button1_xpath" prop="button1_xpath" sortable />
+        </el-table>
+        <el-row type="flex" justify="center" style="margin-top: 10px">
+          <el-col :span="3">
+            <el-button
+              type="primary"
+              icon="el-icon-circle-plus-outline"
+              size="small"
+              round
+              @click="xPathManagerDialogVisible = true"
+            >
+              新增
+            </el-button>
+          </el-col>
+        </el-row>
+      </div>
     </el-card>
+
+    <el-dialog title="新增 X-Path 信息" :visible.sync="xPathInfoDialogVisible">
+      <el-form
+        ref="form"
+        :model="xPathManagerAddForm"
+        label-width="150px"
+        :rules="xPathManagerAddFormRules"
+      >
+
+      </el-form>
+    </el-dialog>
+
   </el-main>
 </template>
 
@@ -73,14 +210,37 @@ import {
 } from '@/api/newsCrawlerAll'
 import { addStrategy, getStrategyLs } from '@/api/crawlStrategy'
 import { getAllCrawlerLog } from '@/api/crawlLog'
+import { validUsername } from '@/utils/validate'
 
 export default {
   data() {
     return {
-      tableList: [
-        { type: '12', frameNumber: '23' }
+      presentType0: true,
+      presentType1: true,
+
+      xPathManagerDialogVisible: false,
+      xPathManagerAddForm: {},
+      managetableList: [
+        { type: '12', inSpec: '12' }
       ],
-      managetableList: []
+      tableList: [
+        { type: '1', frameNumber: '23' },
+        { type: '0', frameNumber: '23' },
+        { type: '1', frameNumber: '23' }
+      ],
+
+      xPathManagerAddFormRules: {
+        id: [{ required: true, message: '请输入 ID', trigger: 'blur' }],
+        type: [{ required: true, message: '请输入类型', trigger: 'blur' }],
+        inSpec: [{ required: true, message: '请输入是否在详情页', trigger: 'blur' }],
+        saveName: [{ required: true, message: '请输入数据库存储名称', trigger: 'blur' }],
+        needCrawl: [{ required: true, message: '请输入是否需要爬取', trigger: 'blur' }],
+        EnglishName: [{ required: true, message: '请输入字段名称（英）', trigger: 'blur' }],
+        ChineseName: [{ required: true, message: '请输入字段名称（中）', trigger: 'blur' }],
+        needSave: [{ required: true, message: '请输入是否需要存储', trigger: 'blur' }]
+      },
+
+      xPathInfoDialogVisible: false
     }
   },
   mounted() {
@@ -148,16 +308,29 @@ export default {
           modifyOneSpecXpath(this.token, row)
         }
       }
+    },
+
+    validateXPathManagerAddForm(props, callback) {
+      if (!props.every(text => !text.isEmpty())) {
+        callback('Wrong')
+      }
+    },
+
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.managetableList.push(this.xPathManagerAddForm);
+          this.xPathManagerAddForm = {}
+        } else {
+          return false
+        }
+      })
     }
   }
 }
 </script>
 
 <style scoped>
-.el-row {
-  margin-bottom: 20px;
-}
-
 .tb-edit .el-input {
   display: none;
 }
@@ -168,5 +341,9 @@ export default {
 
 .tb-edit .current-row .el-input + span {
   display: none;
+}
+
+.clearfix {
+  color: cornflowerblue;
 }
 </style>
